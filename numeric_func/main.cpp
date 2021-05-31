22a49d79e039e52e3e71660a6817f112db0c58de
Matías López <milopez3@miuandes.cl>
2021-05-31 14:42:13 -0400

    numeric funtions

---
+++ numeric_func/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    std::cout << sqrt(10) << std::endl;
+    std::cout << remainder(10, 3) << std::endl;
+    std::cout << fmod(10, 3) << std::endl;
+}
